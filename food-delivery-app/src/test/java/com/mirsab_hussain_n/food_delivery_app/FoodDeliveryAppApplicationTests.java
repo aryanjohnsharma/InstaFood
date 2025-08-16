3723d596ba5a0b82d6e456fa25ac56cd05b5bcb1
Aryan Sharma <97747980+aryanjohnsharma@users.noreply.github.com>
2025-08-16 12:22:21 +0530

    new stuff

--- food-delivery-app/src/test/java/com/mirsab_hussain_n/food_delivery_app/FoodDeliveryAppApplicationTests.java
+++ food-delivery-app/src/test/java/com/mirsab_hussain_n/food_delivery_app/FoodDeliveryAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mirsab_hussain_n.food_delivery_app;
+package com.aryansharma_n.food_delivery_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
